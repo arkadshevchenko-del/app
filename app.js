959d4270dfe1770c1ed8d6c29488bb50862f9b7e
arkadshevchenko-del <arkad.shevchenko@gmail.com>
2026-07-04 13:55:53 +0300

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const fs = require("fs")
 const app = express()
 const PORT = process.env.PORT || 3000
 let chat = []
+app.use(express.json());
 app.get("/",(req,res) => {
   res.sendFile(path.join(__dirname,"index.html"))
 })
@@ -11,7 +12,7 @@ app.get("/getChat",(req,res) => {
   res.json(chat)
 })
 app.post("/addToChatMessege",(req,res)=>{
-  
+
   chat.push({name:req.body.name,message:req.body.message})
   res.json()
 })
